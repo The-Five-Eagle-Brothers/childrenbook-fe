3a1cb82cf1b8f0d9229b1bb9fa7acf3fdb6e3682
intersoom <sallybig@naver.com>
2023-06-28 22:55:39 +0900

    🎉 project setting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-import React, { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import Intro from "./scene/Intro";
-import Sunset from "./scene/Sunset";
-import Sunset2 from "./scene/Sunset2";
-import Essential from "./scene/Essential";
-import WaveScene from "./scene/WaveScene";
-import WaveVideo from "./scene/WaveVideo";
-import EndingVideo from "./scene/EndingVideo";
-import WaveScene2 from "./scene/WaveScene2";
-import Tutorial from "./scene/Tutorial";
-import VideoIntro from "./scene/VideoIntro";
-import SnakeEnd from "./scene/SnakeEnd";
-import Start from "./scene/Start";
+import React from "react";
 
 function App() {
   return <></>;
