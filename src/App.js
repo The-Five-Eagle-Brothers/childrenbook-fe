78b4bbff0652ba7368e54b3518b0eea8edd5d929
intersoom <sallybig@naver.com>
2023-10-28 01:58:21 +0900

    feat: 카카오 소셜 로그인

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Start from "./components/Start";
 import LittlePrince from "./components/LittlePrince";
 import RabbitAndTurtle from "./components/RabbitAndTurtle";
 import VideoIntro from "./components/VideoIntro";
+import Redirection from "./components/Redirection";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/main" element={<Main />}></Route>
-        <Route path="/" element={<Start />}></Route>
-        <Route path="/littlePrince" element={<LittlePrince />}></Route>
-        <Route path="/rabbitAndTurtle" element={<RabbitAndTurtle />}></Route>
-        <Route path="/rabbitAndTurtleIntro" element={<VideoIntro />}></Route>
+        <Route path="/main" element={<Main />} />
+        <Route path="/" element={<Start />} />
+        <Route path="/littlePrince" element={<LittlePrince />} />
+        <Route path="/rabbitAndTurtle" element={<RabbitAndTurtle />} />
+        <Route path="/rabbitAndTurtleIntro" element={<VideoIntro />} />
+        <Route exact path="/kakao/callback" element={<Redirection />} />
       </Routes>
     </>
   );
